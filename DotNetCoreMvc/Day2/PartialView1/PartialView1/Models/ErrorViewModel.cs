74b9ffcbf71c0c70ab97b3f4bbd50c8c95f5e9aa
Suraj-Prakash01 <79893175+Suraj-Prakash01@users.noreply.github.com>
2021-03-07 23:37:06 +0530

    hands on

---
+++ DotNetCoreMvc/Day2/PartialView1/PartialView1/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PartialView1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
