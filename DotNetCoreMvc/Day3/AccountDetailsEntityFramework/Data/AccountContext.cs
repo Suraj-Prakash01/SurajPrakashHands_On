c5fc21a239ec01421b1821685fa89146c127f0be
Suraj-Prakash01 <79893175+Suraj-Prakash01@users.noreply.github.com>
2021-03-08 14:08:42 +0530

    hands on

---
+++ DotNetCoreMvc/Day3/AccountDetailsEntityFramework/Data/AccountContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using AccountDetailsEntityFramework.Models;
+
+namespace AccountDetailsEntityFramework.Data
+{
+    public class AccountContext:DbContext
+    {
+        public AccountContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
