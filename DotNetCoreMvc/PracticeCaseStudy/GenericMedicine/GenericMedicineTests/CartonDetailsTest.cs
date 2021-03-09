e4cf718d3385f64c5accac571a20ece237856cfb
Suraj-Prakash01 <79893175+Suraj-Prakash01@users.noreply.github.com>
2021-03-09 20:06:59 +0530

    hands on

---
+++ DotNetCoreMvc/PracticeCaseStudy/GenericMedicine/GenericMedicineTests/CartonDetailsTest.cs
@@ -0,0 +1,83 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GenericMedicine;
+
+namespace GenericMedicineTests
+{
+    [TestFixture]
+   
+    class CartonDetailsTest
+    {
+        Program p;
+        Medicine m = new Medicine()
+        {
+            Name = "Name1",
+                GenericMedicineName = "Gen_Med_Name",
+                Composition = "Comp1",
+                ExpiryDate = new DateTime(2022, 12, 1),
+                PricePerStrip = 30
+             };
+
+        [OneTimeSetUp]
+        public void init()
+        {
+            p = new Program();
+            
+            
+        }
+        [Test]
+        [TestCase(1,"2022/6/3","Jehanabad")]
+        public void CreateCartonDetail_WhenCalled_ReturnCartonObject(int medicineStripCount, DateTime launchDate, string retailerAddress)
+        {
+            CartonDetail result = p.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, m);
+            Assert.That(result.MedicineStripCount, Is.EqualTo(medicineStripCount));
+            Assert.That(result.LaunchDate, Is.EqualTo(launchDate));
+            Assert.That(result.RetailerAddress, Is.EqualTo(retailerAddress));
+            Assert.That(result.Medicine, Is.EqualTo(m));
+        }
+        [Test]
+        [TestCase(-1, "2022/6/3", "Jehanabad")]
+        public void CreateCartonDetail_WhenStripCountIsLessThanZero_ThrowsException(int medicineStripCount, DateTime launchDate, string retailerAddress)
+        {
+            var exception = Assert.Throws<Exception>(() => p.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress,m));
+            Assert.AreEqual("Incorrect strip count. Please check", exception.Message);
+        }
+        [Test]
+        [TestCase(1, "2020/6/3", "Jehanabad")]
+        public void CreateCartonDetail_WhenLaunchDateIsLessThanCurrentDate_ThrowsException(int medicineStripCount, DateTime launchDate, string retailerAddress)
+        {
+           
+
+            var exception = Assert.Throws<Exception>(() => p.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress,m));
+            Assert.AreEqual("Incorrect launch date. Please provide valid value", exception.Message);
+
+        }
+
+        [Test]
+        [TestCase(1, "2024/6/3", "Jehanabad")]
+        public void CreateCartonDetail_WhenLaunchDateIsGreaterThanExpiryDate_ThrowsException(int medicineStripCount, DateTime launchDate, string retailerAddress)
+        {
+
+
+            var exception = Assert.Throws<Exception>(() => p.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, m));
+            Assert.AreEqual("Launch date greater than expiry date. Please check", exception.Message);
+
+        }
+        [Test]
+        [TestCase(1, "2020/6/3", "Jehanabad")]
+        public void CreateCartonDetail_WhenMedicineObjectValueIsNull_ThrowsException(int medicineStripCount, DateTime launchDate, string retailerAddress)
+        {
+
+
+            CartonDetail result= p.CreateCartonDetail(medicineStripCount, launchDate, retailerAddress, null);
+            Assert.That(result, Is.EqualTo(null));
+
+        }
+
+
+    }
+}
---
+++ DotNetCoreMvc/PracticeCaseStudy/GenericMedicine/GenericMedicineTests/MedicineDetailsTest.cs
@@ -0,0 +1,61 @@
+﻿using GenericMedicine;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericMedicineTests
+{
+    [TestFixture]
+    public class MedicineDetailsTest
+    {
+        [Test]
+        [TestCase("Name1", "Gen_med_1", "Pharma1", "2022/6/3", 12)]
+        [TestCase("Name2", "Gen_med_2", "Pharma2", "2023/11/13", 11.9)]
+        [TestCase("Name3", "Gen_med_3", "Pharma3", "2021/11/6", 110.11)]
+        public void CreateMedicineDetail_WhenCalled_ReturnMedicineObject(string name, string genericMedicineName, string composition, DateTime expiryDate, double pricePerStrip)
+        {
+            Program p = new Program();
+            Medicine result = p.CreateMedicineDetail(name, genericMedicineName, composition, expiryDate, pricePerStrip);
+
+            Assert.That(result.Name, Is.EqualTo(name));
+            Assert.That(result.GenericMedicineName, Is.EqualTo(genericMedicineName));
+            Assert.That(result.Composition, Is.EqualTo(composition));
+            Assert.That(result.ExpiryDate, Is.EqualTo(expiryDate));
+            Assert.That(result.PricePerStrip, Is.EqualTo(pricePerStrip));
+        }
+        [Test]
+        [TestCase("Name1", "", "Pharma1", "2022/6/3", 12)]
+        [TestCase("Name2", null, "Pharma2", "2023/11/13", 11.9)]
+        public void CreateMedicineDetail_WhenCalledWithEmptyGenericMedicineName_ThrowsException(string name, string genericMedicineName, string composition, DateTime expiryDate, double pricePerStrip)
+        {
+            Program p = new Program();
+
+            var exception = Assert.Throws<Exception>(() => p.CreateMedicineDetail(name, genericMedicineName, composition, expiryDate, pricePerStrip));
+            Assert.AreEqual("Generic Medicine name cannot be empty. Please provide valid value", exception.Message);
+        }
+        [Test]
+        [TestCase("Name1", "Gen_med_1", "Pharma1", "2022/6/3", -12)]
+        [TestCase("Name2", "Gen_med_2", "Pharma2", "2023/11/13", -11.9)]
+        public void CreateMedicineDetail_WhenCalledWithPriceZero_ThrowsException(string name, string genericMedicineName, string composition, DateTime expiryDate, double pricePerStrip)
+        {
+            Program p = new Program();
+
+            var exception = Assert.Throws<Exception>(() => p.CreateMedicineDetail(name, genericMedicineName, composition, expiryDate, pricePerStrip));
+            Assert.AreEqual("Incorrect value for Medicine price per strip. Please provide valid value", exception.Message);
+        }
+        [Test]
+        [TestCase("Name1", "Gen_med_1", "Pharma1", "2020/6/3", 12)]
+        [TestCase("Name2", "Gen_med_2", "Pharma2", "2019/11/13", 11.9)]
+        public void CreateMedicineDetail_WhenCalledWithDateLessThanCurrentDate_ThrowsException(string name, string genericMedicineName, string composition, DateTime expiryDate, double pricePerStrip)
+        {
+            Program p = new Program();
+
+            var exception = Assert.Throws<Exception>(() => p.CreateMedicineDetail(name, genericMedicineName, composition, expiryDate, pricePerStrip));
+            Assert.AreEqual("Incorrect expiry date. Please provide valid value", exception.Message);
+        }
+       
+    }
+}
