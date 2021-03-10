4ce0695699796f67802b33ac24b475a24cb7d2f5
Suraj-Prakash01 <79893175+Suraj-Prakash01@users.noreply.github.com>
2021-03-10 20:50:45 +0530

    handson

---
+++ DotNetCoreMvc/CaseStudy/MyFoodSupply/MyFoodSupplyTests/FoodDetailTest.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyFoodSupply;
+
+namespace MyFoodSupplyTests
+{
+    [TestFixture]
+    public class FoodDetailTest
+    {
+        Program p;
+        [OneTimeSetUp]
+        public void init()
+        {
+            p = new Program();
+        }
+       
+        [Test]
+        [TestCase("Name1",1,"2022/6/3",500.00)]
+        public void CreateFoodDetail_WhenCalled_ReturnFoodDetailObject(string name, int dishType, DateTime expiryDate, double price)
+        {
+            
+            FoodDetail result = p.CreateFoodDetail(name, dishType, expiryDate, price);
+
+            Assert.That(result.Name, Is.EqualTo(name));
+            Assert.That(result.DishType, Is.EqualTo((FoodDetail.Category)dishType));
+            Assert.That(result.ExpiryDate, Is.EqualTo(expiryDate));
+            Assert.That(result.Price, Is.EqualTo(price));
+        }
+
+        [Test]
+        [TestCase(null, 1, "2022/6/3", 500.00)]
+        [TestCase("", 1, "2022/6/3", 500.00)]
+        public void CreateFoodDetail_WhenFoodItemNameIsNull_ThrowsException(string name, int dishType, DateTime expiryDate, double price)
+        {
+            var exception= Assert.Throws<Exception>(() => p.CreateFoodDetail(name, dishType, expiryDate, price));
+            Assert.AreEqual("Dish name cannot be empty. Please provide valid value", exception.Message);
+        }
+
+        [Test]
+        [TestCase("Name1", 1, "2022/6/3", -500.00)]
+        public void CreateFoodDetail_WhenPriceIsLessThanZero_ThrowsException(string name, int dishType, DateTime expiryDate, double price)
+        {
+            var exception = Assert.Throws<Exception>(() => p.CreateFoodDetail(name, dishType, expiryDate, price));
+            Assert.AreEqual("Incorrect value for dish price. Please provide valid value", exception.Message);
+        }
+        [Test]
+        [TestCase("Name1", 1, "2019/6/3", 500.00)]
+        public void CreateFoodDetail_WhenExpiryDateIsLessThanCurrentDate_ThrowsException(string name, int dishType, DateTime expiryDate, double price)
+        {
+            var exception = Assert.Throws<Exception>(() => p.CreateFoodDetail(name, dishType, expiryDate, price));
+            Assert.AreEqual("Incorrect expiry date. Please provide valid value", exception.Message);
+        }
+    }
+}
---
+++ DotNetCoreMvc/CaseStudy/MyFoodSupply/SupplyDetailTest/CreateSupplyTest.cs
@@ -0,0 +1,63 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyFoodSupply;
+
+namespace SupplyDetailTest
+{
+    [TestFixture]
+    public class CreateSupplyTest
+    {
+        Program p;
+        FoodDetail f= new FoodDetail()
+        {
+            Name = "name1",
+            DishType = (FoodDetail.Category)1,
+            Price = 500,
+            ExpiryDate = new DateTime(2022,6,3)
+        };
+        [OneTimeSetUp]
+        public void init()
+        {
+            p = new Program();
+        }
+        [Test]
+        [TestCase(1,"2021/6/3","Rohit",50.0)]
+        public void CreateSupplyDetail_WhenCalled_ReturnSupplyDetailObject(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            SupplyDetail result = p.CreateSupplyDetail(foodItemCount, requestDate, sellerName,packingCharge,f);
+            
+            Assert.That(result.Count, Is.EqualTo(foodItemCount));
+            Assert.That(result.RequestDate, Is.EqualTo(requestDate));
+            Assert.That(result.SellerName, Is.EqualTo(sellerName));
+            //Assert.That(result.PackingCharge, Is.EqualTo(packingCharge));
+            Assert.That(result.FoodItem, Is.EqualTo(f));
+        }
+        [Test]
+        [TestCase(-1, "2021/6/3", "Rohit", 50.0)]
+        public void CreateSupplyDetail_WhenFoodCountLessThanZero_ThrowsException(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            var exception = Assert.Throws<Exception>(() => p.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, f));
+            Assert.AreEqual("Incorrect food item count. Please check",exception.Message);
+        }
+
+        [Test]
+        [TestCase(1, "2020/6/3", "Rohit", 50.0)]
+        public void CreateSupplyDetail_WhenRequestDateLessThanCurrentDate_ThrowsException(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            var exception = Assert.Throws<Exception>(() => p.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, f));
+            Assert.AreEqual("Incorrect food request date. Please provide valid value", exception.Message);
+        }
+
+        [Test]
+        [TestCase(1, "2021/6/3", "Rohit", 50.0)]
+        public void CreateSupplyDetail_WhenFoodItemObjectIsNull_ReturnNull(int foodItemCount, DateTime requestDate, string sellerName, double packingCharge)
+        {
+            var result =  p.CreateSupplyDetail(foodItemCount, requestDate, sellerName, packingCharge, null);
+            Assert.AreEqual(null,result) ;
+        }
+    }
+}
